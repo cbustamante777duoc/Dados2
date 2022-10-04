ceafc93f18b5ea7837f6997a46e4b243fc99da86
cbustamante777 <cra.bustamante@alumnos.duoc.cl>
2022-10-04 20:04:34 -0300

    app done Dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice2.png';
+
+  numero1: number = 1;
+  numero2: number = 2;
+
+  tirarDados(): void {
+
+    //generar numero random
+    this.numero1 = Math.round(Math.random() * 5 + 1);
+    this.numero2 = Math.round(Math.random() * 5 + 1);
+
+    //cambia la img de los dados
+    this.dadoIzquierda = `../assets/img/dice${this.numero1}.png`;
+    this.dadoDerecha = `../assets/img/dice${this.numero2}.png`;
+
+  }
 }
